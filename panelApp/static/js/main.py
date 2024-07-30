c92f24830ec8d7f960d53181ffe0804e0a00ad8d
liwBh <liwbarqueroh@gmail.com>
2024-07-30 12:17:54 -0600

    agregando css al panel

--- panelApp/static/js/main.py
+++ panelApp/static/js/main.py
@@ -1,15 +1,33 @@
-import panel as pn
 from pyscript import display
+import panel as pn
+import numpy as np
 
-display("Hello, World!", target="panel")
+pn.extension()
 
-pn.extension(sizing_mode="stretch_width")
-
-slider = pn.widgets.FloatSlider(start=0, end=10, name='Amplitude')
-
-
-def callback(new):
-    return f'Amplitude is: {new}'
+# Definir sliders para controlar el ancho y la altura
+width_slider = pn.widgets.IntSlider(name='Width', start=100, end=500, step=10, value=500)
+height_slider = pn.widgets.IntSlider(name='Height', start=100, end=500, step=10, value=500)
 
 
-pn.Row(slider, pn.bind(callback, slider)).servable(target='simple_app')
+# Panel de dibujo
+@pn.depends(width_slider.param.value, height_slider.param.value)
+def update_drawing_panel(width, height):
+    # Crear el panel de dibujo con el tamaño especificado
+    return pn.pane.HTML(f'<div style="width:{width}px; height:{height}px;" class="panel"></div>')
+
+
+# Configurar el sidebar
+sidebar = pn.Column(
+    width_slider,
+    height_slider,
+    css_classes=['sidebar']
+)
+
+# Configurar layout principal
+layout = pn.Row(
+    sidebar,
+    update_drawing_panel
+)
+
+# Servir el layout
+layout.servable(target='control-panel')
