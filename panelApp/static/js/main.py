c244fb828740ef5177278ff201a66ea56a50a0ca
liwBh <liwbarqueroh@gmail.com>
2024-07-30 14:39:49 -0600

    color de fondo svg

--- panelApp/static/js/main.py
+++ panelApp/static/js/main.py
@@ -6,19 +6,20 @@ pn.extension()
 # Definir sliders para controlar el ancho y la altura
 width_slider = pn.widgets.IntSlider(name='Width', start=100, end=500, step=10, value=500)
 height_slider = pn.widgets.IntSlider(name='Height', start=100, end=500, step=10, value=500)
+color_picker = pn.widgets.ColorPicker(name='Color Picker', value='#ffffff')
 
 # SVG base en blanco
-def create_svg_base(width, height):
+def create_svg_base(width, height, color):
     return f'''
     <svg id="drawing-svg" width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="{height}" fill="white"/>
+      <rect width="100%" height="{height}" fill="{color}"/>
     </svg>
     '''
 
 # Panel de dibujo que se actualiza con los sliders
-@pn.depends(width_slider.param.value, height_slider.param.value)
-def update_drawing_panel(width, height):
-    svg_content = create_svg_base(width, height)
+@pn.depends(width_slider.param.value, height_slider.param.value, color_picker.param.value)
+def update_drawing_panel(width, height, color):
+    svg_content = create_svg_base(width, height, color)
     return pn.pane.HTML(f'<div class="panel">{svg_content}</div>', width=width, height=height)
 
 
@@ -26,6 +27,7 @@ def update_drawing_panel(width, height):
 sidebar = pn.Column(
     width_slider,
     height_slider,
+    color_picker,
     css_classes=['sidebar']
 )
 
