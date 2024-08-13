ce8242fa75b42b8274d74b671eb6197aaff6c19c
liwBh <liwbarqueroh@gmail.com>
2024-08-13 11:03:24 -0600

    shadow root

--- src/panelApp/static/panelApp/pyscript/main.py
+++ src/panelApp/static/panelApp/pyscript/main.py
@@ -1,10 +1,11 @@
 # dependencias
 import xml.etree.ElementTree as ET
 from io import StringIO
-from js import document, console
-#from panel.reactive import ReactiveHTML
+
+# from panel.reactive import ReactiveHTML
 import panel as pn
 import param
+from js import document, console
 
 # archivos
 import figures
@@ -32,14 +33,12 @@ class SVGParams(param.Parameterized):
         self.param.trigger("current_shape")
         self.param.trigger("is_load_svg")
 
-
     def get_all_shapes(self):
         all_shapes = ""
         for shape in self.additional_shapes:
             all_shapes += shape.get_svg() + "\n"
 
         return all_shapes
-
 
     # def update_current_shape(self, shape_id):
     #     print(f'Updating current shape to {shape_id}')
@@ -88,6 +87,7 @@ class SVGParams(param.Parameterized):
         for f in self.additional_shapes:
             print(f.get_svg())
 
+
 # import param
 # from panel.reactive import ReactiveHTML
 # class ReactiveFigures(ReactiveHTML):
@@ -122,7 +122,7 @@ height_slider = pn.widgets.IntSlider(name='Height', start=100, end=500, step=10,
 # Selección de color de fondo
 color_picker = pn.widgets.ColorPicker(name='Background color', value='#ffffff')
 # Campo de entrada para agregar texto al SVG
-#text_input = pn.widgets.TextInput(name='Text to Add', value='I love SVG!', max_length=250, width=240)
+# text_input = pn.widgets.TextInput(name='Text to Add', value='I love SVG!', max_length=250, width=240)
 text_input = pn.widgets.TextInput(name='Text to Add', value='', max_length=250, width=240)
 # Botón para actualizar el SVG con el texto
 text_size = pn.widgets.IntSlider(name='Font size', start=8, end=24, step=1, value=12, width=240)
@@ -133,7 +133,7 @@ file_input = pn.widgets.FileInput(accept='.svg', multiple=False, width=240)
 # Boton para limpiar el contenido del SVG
 clear_svg_button = pn.widgets.Button(name='Clear SVG', icon='trash', button_type='danger', height=30, width=240)
 
-
+temp_button = pn.widgets.Button(name='Prueba', button_type='warning', width=240)
 # ========================================================================================
 # Variable Globales
 # ========================================================================================
@@ -151,6 +151,61 @@ def select_svg_element_by_id(element_id):
             console.error(f"No element found with id {element_id}.")
     except Exception as e:
         console.error(f"Error selecting element: {e}")
+
+
+# ========================================================================================
+# ShadowRoot
+# ========================================================================================
+def unlock_shadowroot(element_id, shadow_root=None):
+    # Access the outer shadow root host
+    outer_host = document.querySelector(element_id)
+
+    if shadow_root is not None:
+        outer_host = shadow_root.querySelector(element_id)
+
+    # Create the outer shadow root if it doesn't already exist
+    if not outer_host.shadowRoot:
+        outer_host.attachShadow({'mode': 'open'})
+
+    # Get the outer shadow root
+    return outer_host.shadowRoot
+
+def prueba_xd(event):
+    console.log('Prueba')
+
+#def unlock_panel(event):
+def unlock_panel(event):
+    try:
+        # arreglo de los shadow roots a deshabilitar
+        shadow_roots = ['.bk-panel-models-layout-Column', '.bk-panel-models-layout-Column', '.bk-panel-models-markup-HTML']
+
+        tem_shadow_root = None
+
+        for shadow_root in shadow_roots:
+            tem_shadow_root = unlock_shadowroot(shadow_root, tem_shadow_root)
+
+        # llegar a la figura objetivo
+        figure = tem_shadow_root.querySelector('#background')
+        figure.onclick = lambda event: prueba_xd(event)
+        figure.setAttribute('stroke', 'blue')
+        figure.setAttribute('stroke-width', 1)
+
+        #figure.style.border = '2px solid red'
+
+        # iterar el arreglo de figuras
+        # obtener su id y setearla en una variable global
+        # agregarle una funcion
+        # resaltar la figura    "agregar stylos por medio de un atributo nuevo en la figura"
+
+        print(figure)
+        console.log(figure)
+    except Exception as e:
+        console.error(f"Error unlocking panel: {e}")
+
+
+
+
+temp_button.on_click(unlock_panel)
 
 
 # ========================================================================================
@@ -176,7 +231,6 @@ def wrap_text(text, max_width, font_size, text_color):
     if current_line:
         lines.append(current_line)
 
-
     text_elements = ''.join(
         f'<tspan x="5" dy="{font_size + 5}" fill="{text_color}">{line}</tspan>'
         if i != 0 else f'<tspan x="5" dy="0" fill="{text_color}">{line}</tspan>'
@@ -185,15 +239,16 @@ def wrap_text(text, max_width, font_size, text_color):
 
     return text_elements
 
+
 # SVG base en blanco
 def create_svg_base(width, height, color, text, font_size, text_color, shapes, is_load_svg):
-
     # Ajustar las lineas del texto al ancho del SVG
     wrapped_text = wrap_text(text, width - 10, font_size, text_color)
 
     if not is_load_svg and (len(shapes) == 0 or shapes[0].id != 'background'):
         factory = figures.FigureFactory()
-        background = factory.create_element("rect", id="background", x=0, y=0, width=width, height=height, fill=color, stroke='none', stroke_width=0)
+        background = factory.create_element("rect", id="background", x=0, y=0, width=width, height=height, fill=color,
+                                            stroke='none', stroke_width=0)
         shapes.insert(0, background)
 
     # Crear un string con las figuras adicionales
@@ -215,12 +270,10 @@ def create_svg_base(width, height, color, text, font_size, text_color, shapes, i
     text_input.param.value,
     text_size.param.value,
     text_color_picker.param.value,
-
     svg_params.param.additional_shapes,
     svg_params.param.is_load_svg,
 )
-def create_panel(width, height, color, text, font_size, text_color, shapes, is_load_svg,):
-
+def create_panel(width, height, color, text, font_size, text_color, shapes, is_load_svg, ):
     svg_params.svg_content = create_svg_base(width, height, color, text, font_size, text_color, shapes, is_load_svg)
     svg_content = create_svg_base(width, height, color, text, font_size, text_color, shapes, is_load_svg)
     # return pn.pane.SVG(
@@ -245,8 +298,10 @@ def select_figure(event):
     svg_params.update_current_shape(shape_id)
     print(f'Selected shape ID: {shape_id}')
 
+
 # Función para agregar figuras al SVG
 def add_figure(event):
+    #unlock_panel(event)
     button = event.obj
     factory = figures.FigureFactory()
     type_figure = ""
@@ -269,8 +324,8 @@ def add_figure(event):
     svg_params.additional_shapes.append(figure)
     svg_params.param.trigger('additional_shapes')
     svg_params.param.trigger('svg_content')
-
-    #svg_params.update_current_shape(id_figure)
+    unlock_panel(event)
+    # svg_params.update_current_shape(id_figure)
 
 
 # Crear menu de botones para agregar figuras
@@ -288,7 +343,8 @@ def create_button_panel(buttons):
     # Returnar panel con botones
     return pn.Row(*button_widgets)
 
-#@pn.depends(file_input.param.value, watch=True)
+
+# @pn.depends(file_input.param.value, watch=True)
 def handle_file_upload(event):
     color_picker.value = '#FFFFFF'
     file_data = event.new
@@ -310,17 +366,16 @@ def handle_file_upload(event):
     else:
         print("Archivo subido es None")
 
+
 # Funcion para actualizar el color de fondo del SVG
 def update_background_color(event):
     if len(svg_params.additional_shapes) > 0:
         svg_params.additional_shapes[0].update(fill=color_picker.value)
 
+
 # Conectar la función de manejo de archivos al widget
 file_input.param.watch(handle_file_upload, 'value')
 color_picker.param.watch(update_background_color, 'value')
-
-
-
 
 # ========================================================================================
 # Configuraciones elementos de interfaz
@@ -346,8 +401,10 @@ settings_tab = pn.Column(
     download_svg,
     file_input,
     clear_svg_button,
+    temp_button,
     name='Setting'
 )
+
 
 def clear_svg_and_file_input(event):
     svg_params.clear_svg()
@@ -375,9 +432,9 @@ figures_tab = pn.Column(
 
 # Tab con las configuraciones de los pictogramas
 pictogram_tab = pn.Column(name='Pictogram')
-#xd = ReactiveFigures()
+# xd = ReactiveFigures()
 # Panel principal con el SVG
-#panel = pn.Column(xd, styles=utils.styles_panel)
+# panel = pn.Column(xd, styles=utils.styles_panel)
 panel = pn.Column(create_panel, styles=utils.styles_panel)
 
 # Panel con la barra lateral "Sidebar contiene los tabs"
@@ -401,3 +458,10 @@ sidebar = pn.Tabs(
 sidebar.servable(target='sidebar-panel')
 panel.servable(target='drawing-panel')
 
+# Ejecutar unlock_panel cuando la página esté cargada
+#pn.state.onload(unlock_panel)
+#pn.state.onload(lambda: unlock_panel())
+#unlock_panel()
+# cargar de forma predeterminada no es posible
+# opcion: activar la funcion unlock_panel cada vez que se modifica el contenido del SVG
+#svg_params.param.watch(unlock_panel, 'svg_content')
